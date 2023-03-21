d1aef6500fc85dc839ee6895ef9cff836a0ad28a
vadim <ivinjc@gmail.com>
2023-03-21 20:22:15 +0200

    Завершение проекта по изменению состояния из дочерних компонент

--- 12-state-via-props/src/App.js
+++ 12-state-via-props/src/App.js
@@ -1,16 +1,20 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import Counter from "./components/Counter";
+
 function App() {
   const [count, setCount] = useState(10);
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
-      <Counter count={count}/>
-      <Button count={count} onClick={setCount}/>
-      <Button count={count} onClick={setCount}/>
-      <Button count={count} onClick={setCount}/>
-      <Button count={count} onClick={setCount}/>
+      <Counter count={count} />
+      <Button onClick={incrementCounter} />
+      <Button onClick={incrementCounter} />
+      <Button onClick={incrementCounter} />
+      <Button onClick={incrementCounter} />
     </div>
   );
 }
--- 12-state-via-props/src/components/Button.js
+++ 12-state-via-props/src/components/Button.js
@@ -1,5 +1,5 @@
-function Button({count, onClick}) {
-  return <button onClick={() => onClick(count + 1)}>Click me</button>
+function Button({onClick}) {
+  return <button onClick={onClick}>Click me</button>
 }
 
 export default Button;
