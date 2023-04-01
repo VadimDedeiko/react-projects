c7c55b6f3227795b7f0a093daf93409cba4fdc61
vadim <ivinjc@gmail.com>
2023-04-01 20:00:44 +0300

    198. Добавление индикации загрузки данных.ts

--- 21-jsonplaceholder-post/src/components/Posts.js
+++ 21-jsonplaceholder-post/src/components/Posts.js
@@ -4,14 +4,18 @@ import React, { useEffect, useState } from "react";
 function Posts() {
   const [posts, setPosts] = useState([])
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((posts) => {
         console.log(posts);
         setPosts(posts)
-      }).catch((err) => setError(err.message));
+      }).catch((err) => setError(err.message)).finally(() => setIsLoading(false));
   }, []);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   if (error) {
     return <p>Error {error}</p>;
   }
