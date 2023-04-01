68a2244e23524c690f5d966b9472d185bcf6548e
vadim <ivinjc@gmail.com>
2023-04-01 19:16:57 +0300

    195. РЕШЕНИЕ - Отображение массива постов в интерфейсе.ts

--- 21-jsonplaceholder-post/src/components/Post.js
+++ 21-jsonplaceholder-post/src/components/Post.js
@@ -1,5 +1,14 @@
-function Post() {
-  return <h1>This is a Post!</h1>
+import './Post.css'
+function Post(props) {
+  const { id, title, userId, body } = props;
+  return (
+    <div className="post">
+      <small>{id}</small>
+      <h1>{title}</h1>
+      <p>User ID: {userId}</p>
+      <h2>{body}</h2>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
--- 21-jsonplaceholder-post/src/components/Posts.js
+++ 21-jsonplaceholder-post/src/components/Posts.js
@@ -1,21 +1,23 @@
 import Post from "./Post";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Posts() {
   const [posts, setPosts] = useState([])
   useEffect(() => {
-    fetch("https://jslaceholder.typicode.com/posts")
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((posts) => {
         console.log(posts);
         setPosts(posts);
       })
-      .catch((error) => console.log(error.message));
   }, []);
   return (
     <div>
-      <Post />
-      <Post />
+      {
+        posts.map((post) =>
+          (<Post key={post.id} {...post}></Post>)
+        )
+      }
     </div>
   )
 }
