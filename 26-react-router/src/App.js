1c350118c7ddca1316affcb976cdbc72f1f0270e
vadim <ivinjc@gmail.com>
2023-04-08 15:24:37 +0300

    263. Создание компонента Menu.ts

--- 26-react-router/src/App.js
+++ 26-react-router/src/App.js
@@ -1,10 +1,22 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import React from "react";
+import Home from "./components/Home";
+import Contacts from "./components/Contacts";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App"></div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element= {<Home/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="contacts" element={<Contacts/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ 26-react-router/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Menu from "./Menu";
+
+const Home = () => {
+  return (
+    <>
+      <Menu />
+      <h1>Home</h1>
+    </>
+  );
+};
+export default Home;
