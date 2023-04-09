1bea6401d575b846b2b954197920c33d5e1daa69
vadim <ivinjc@gmail.com>
2023-04-09 11:22:52 +0300

    279. Перенаправление с помощью хука useNavigate.ts

--- 26-react-router/src/components/SingleCourses.jsx
+++ 26-react-router/src/components/SingleCourses.jsx
@@ -1,19 +1,27 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import courses from "../data/courses";
-import NotFound from "./NotFound";
 
 const SingleCourses = () => {
   const params = useParams();
+  const navigate = useNavigate()
   const course = courses.find((course) => course.slug === params.slug);
-  if (!course) {
+
+  useEffect(() => {
+    if (!course) {
+      navigate("..", { relative: "path" });
+    }
+  }, [course, navigate]
+)
+ // simple not found component
+  /*if (!course) {
     return <NotFound />
-  }
+  }*/
   return (
     <>
-      <h1>{course.title}</h1>
-      <h2>{course.slug}</h2>
-      <h1>{course.id}</h1>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h1>{course?.id}</h1>
       <Link to=".." relative="path">All courses</Link>
     </>
   );
